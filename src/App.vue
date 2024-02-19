83b3b7cb4cfce755c9bddd2a11a6a7f2960f4cae
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-02-19 15:20:43 -0300

    feat: add base button and link

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <script setup lang="ts">
 	import BaseHeader from './ui/components/base-header.vue'
+	import BaseLink from './ui/atoms/base-link.vue'
 </script>
 <template>
-	<BaseHeader />
+	<BaseHeader>
+		<template #right>
+			<section class="header__authentication-links">
+				<BaseLink title="Sign Up" text>Sign Up</BaseLink>
+				<BaseLink title="Login">Log In</BaseLink>
+			</section>
+		</template>
+	</BaseHeader>
 </template>
 
-<style scoped></style>
+<style scoped>
+	.header__authentication-links {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		gap: 30px;
+	}
+</style>
---
+++ src/ui/atoms/base-link.vue
@@ -0,0 +1,23 @@
+<template>
+	<a :class="`button ${text ? 'link--text' : 'link--button'}`">
+		<slot></slot>
+	</a>
+</template>
+
+<script setup lang="ts">
+	defineProps<{ text?: boolean }>()
+</script>
+
+<style scoped>
+	.link--text {
+		width: auto;
+		height: auto;
+		background: none;
+		color: var(--white);
+	}
+	.link--button {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
